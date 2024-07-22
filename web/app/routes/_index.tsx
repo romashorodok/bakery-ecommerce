bf532fda51445abf2ef96bdb0a6216acbac66e0a
romashorodok <108732029+romashorodok@users.noreply.github.com>
2024-07-23 02:11:32 +0300

    feat: Init client side

---
+++ web/app/routes/_index.tsx
@@ -0,0 +1,46 @@
+import { json, LoaderFunctionArgs, type MetaFunction } from "@remix-run/cloudflare";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    {
+      name: "description",
+      content: "Welcome to Remix on Cloudflare Workers!",
+    },
+  ];
+};
+
+export const loader = async ({ context }: LoaderFunctionArgs) => {
+  console.log(context)
+  return json({ test: "test" })
+}
+
+export default function Index() {
+  return (
+    <div className="font-sans p-4">
+      <h1 className="text-3xl">Welcome to Remix on Cloudflare Workers</h1>
+      <ul className="list-disc mt-4 pl-6 space-y-2">
+        <li>
+          <a
+            className="text-blue-700 underline visited:text-purple-900"
+            target="_blank"
+            href="https://remix.run/docs"
+            rel="noreferrer"
+          >
+            Remix Docs
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-blue-700 underline visited:text-purple-900"
+            target="_blank"
+            href="https://developers.cloudflare.com/workers/"
+            rel="noreferrer"
+          >
+            Cloudflare Workers Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
